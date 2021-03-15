409cc229c0f8a3d62035da7a81647dce4f01bc1e
Krazy0 <57904740+krazyyyy@users.noreply.github.com>
2021-03-15 21:40:22 +0500

    Update settings.py

--- FileShare/settings.py
+++ FileShare/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'FileShare.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'FileSharing',
+        'USER': 'krazy',
+        'PASSWORD': 'python123',
+        'HOST': 'mysql-23496-0.cloudclusters.net',
+        'PORT': '23496',
     }
 }
 
